25447c183768c6ea63a374f51207874743c1023a
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-27 12:03:55 +0700

    membuat untuk menginisialisasi aplikasi Express, mengatur middleware untuk parsing body request, dan mengimpor model/database

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./models');
+app.use(express.json());
+app.use(
+    express.urlencoded({ 
+    extended:false,
+    })
+);
+
+
