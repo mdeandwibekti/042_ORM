daaf9c73e1c09ec80c6fb5e828efd1bdfc6bc6aa
mdeandwibekti <mdeandwibekti@gmail.com>
2025-10-27 12:37:09 +0700

    membuat app.post

--- index.js
+++ index.js
@@ -3,21 +3,32 @@ const app = express();
 const port = 3000;
 const db = require('./models');
 app.use(express.json());
-app.use(
-    express.urlencoded({ 
-    extended:false,
-    })
+app.use(express.urlencoded({ 
+    extended: false,
+ })
 );
 
 app.listen(port, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is started on port 3000");
 });
 
-db.sequelize.sync()
-    .then((result) => {
-        app.listen(3000, () => {
-            console.log('server started');
+db.sequelize.sync().then((result) => {
+        app.listen(port, () => {
+            console.log("Server started");
         })
     })
-        .catch((err) => {console.log(err);
+        .catch((err) => {
+            console.log(err);
         });
+
+app.post("/Komik", async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);  
+        res.send(komik);
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+
